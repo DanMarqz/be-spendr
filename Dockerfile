63946f5b4b362c956f3626d2f7723bccaa49e0d4
Daniel <danielarturo.marquez@gmail.com>
2024-09-02 20:34:50 -0400

    nuevos archivos

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,8 @@ RUN pip3 install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
 # Expose port 5000 for the Flask application
 EXPOSE 5000
+ARG DB_NAME
+RUN echo $DB_NAME
 
 # Define the command to run the Flask application using Gunicorn
 # CMD ["gunicorn", "application:app", "-b", "0.0.0.0:5000", "-w", "4"]
---
+++ scripts/push-commit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd ..
+
+# Solicitar los inputs al usuario
+read -p "  ~~~~>_  Por favor, indique los archivos a agregar al commit (. para todos): " commit_file
+read -p "  ~~~~>_  Por favor, indique un mensaje para el commit: " commit_message
+read -p "  ~~~~>_  Por favor, indique la rama donde se enviara su commit: " commit_branch
+
+# Construir los comandos
+add_files="git add $commit_file"
+add_message="git commit -m \'$commit_message\'"
+send_push="git push -u origin $commit_branch"
+
+# Ejecutar los comandos
+echo "  ~~~~>_  Agregando archivos...  "
+echo "$add_files"
+eval $add_files
+
+echo "  ~~~~>_  Agregando mensaje...  "
+echo "$add_message"
+eval $add_message
+
+echo "  ~~~~>_  Enviando commit...  "
+echo "$send_push"
+eval $send_push
